69e109578b0e693c189fe614c0d62486cab4f266
kulklex <kulklex@gmail.com>
2023-05-09 23:46:42 +0100

    More setup on Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 // tailwind.config.js
 
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./screens/**/*.{js,ts,jsx,tsx,mdx}",
+  ], 
   theme: {
     extend: {},
   },
