799461ec71426b7e34d92ae418b98a576332d3ac
kulklex <kulklex@gmail.com>
2023-05-13 22:27:34 +0100

    Added sanity

---
+++ sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'wpn75len',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/restaurant.js
@@ -0,0 +1,62 @@
+import {defineField, defineType} from 'sanity'
+
+export default {
+  name: 'restaurant',
+  title: 'Restaurant',
+  type: 'document',
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Restauarant name",
+      validation: (Role) => Role.required(), 
+    },
+    {
+      name: "short_description",
+      type: "string",
+      title: "Short Description",
+      validation: (Role) => Role.max(200),
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Image of the Restaurant",
+    },
+    {
+      name: "lat",
+      type: "number",
+      title: "Latitude of the Restaurant",
+    },
+    {
+      name: "long",
+      type: "number",
+      title: "Longitude of the Restaurant",
+    },
+    {
+      name: "address",
+      type: "string",
+      title: "Restaurant Address",
+      validation: (Role) => Role.required(),
+    },
+    {
+      name: "rating",
+      type: "number",
+      title: "Enter a Rating from (1-5 Stars)",
+      validation: (Role) => Role.required().min(1).max(5).error("Please enter a value between 1 and 5"),
+    },
+    {
+      name: "genre",
+      title: "Category",
+      validation: (Role) => Role.required(),
+      type: "reference",
+      to: [{type: "reference"}],
+    },
+    {
+      name: "dishes",
+      type: "array",
+      title: "Dishes",
+      of: 
+    },
+  ],
+
+}
