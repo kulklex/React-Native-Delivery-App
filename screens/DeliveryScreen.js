206192c4fcaab91ecd4a1d2023d67e8c8c9f4426
kulklex <kulklex@gmail.com>
2023-07-01 07:29:35 +0100

    Created Delivery Screen

---
+++ screens/DeliveryScreen.js
@@ -0,0 +1,85 @@
+import { View, Text, SafeAreaView, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { useSelector } from 'react-redux'
+import { selectRestaurant } from '../redux/restaurantSlice'
+import { PhoneIcon, XCircleIcon } from 'react-native-heroicons/solid'
+import * as Progress from 'react-native-progress'
+import MapView, { Marker } from 'react-native-maps'
+
+export default function DeliveryScreen() {
+    const navigation = useNavigation()
+    const restaurant = useSelector(selectRestaurant)
+    
+  return (
+    <View className="bg-[#00CC88] flex-1">
+      <SafeAreaView className="z-50">
+        <View className="flex-row justify-between items-center p-5">
+            <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+                <XCircleIcon color="white" size={30} />
+            </TouchableOpacity>
+            <Text className="font-light text-white text-lg">
+                Order Help
+            </Text>
+        </View>
+
+        <View className="bg-white mx-5 my-2 rounded-lg p-6 z-50 shadow-md">
+            <View className="flex-row justify-between">
+                <View>
+                    <Text className="text-lg text-gray-400">
+                        Estimated Arrival
+                    </Text>
+                    <Text className="text-4xl font-bold">
+                        20-30 Minute
+                    </Text>
+                </View>    
+                <Image
+                    source={{
+                        uri: "https://links.papareact.com/fls"
+                    }} className="h-20 w-20"
+                />
+            </View>
+            <Progress.Bar size={30} color='#00CC88' indeterminate={true} />
+            <Text className="mt-3 text-gray-300">
+                Your order at {restaurant?.title} is being prepared
+            </Text>
+        </View>
+      </SafeAreaView>
+
+      <MapView
+        initialRegion={{
+            latitude: restaurant.lat,
+            longitude: restaurant.long,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+        }} mapType='mutedStandard'
+        className="flex-1 -mt-10 z-0"
+      >
+        <Marker 
+            coordinate={{
+                latitude: restaurant.lat,
+                longitude: restaurant.long,
+            }} 
+            title={restaurant.title} description={restaurant.short_description}
+            identifier='origin' pinColor='#00CC88'
+         />
+      </MapView>
+
+      <SafeAreaView className="bg-white flex-row items-center space-x-5 h-28">
+        <Image
+            source={{
+                uri: 'https://links.papareact.com/wru'
+            }}
+            className="h-12 w-12 bg-gray-300 p-4 rounded-full ml-5"
+        />
+        <View className="flex-1">
+            <Text className="text-lg">Dan Stones</Text>
+            <Text className="text-gray-400">Your Rider</Text>
+        </View>
+        <View className="mr-5 flex-row items-center space-x-1">
+            <PhoneIcon size={25} color="#00CC88" />
+        </View>
+      </SafeAreaView>
+    </View>
+  )
+}
